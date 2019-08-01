9c653f5ef89428299c29e8122239dccdfb652b58
Aditi Kumari <aditis.singh22@gmail.com>
2019-08-02 05:48:17 +1000

    updated

--- src/test/java/com/securepay/AppTest.java
+++ src/test/java/com/securepay/AppTest.java
@@ -1,8 +1,16 @@
 package com.securepay;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import junit.framework.TestSuite;  
 
 /**
  * Unit test for simple App.
@@ -16,6 +24,7 @@ public class AppTest extends TestCase {
 	 */
 	public AppTest(String testName) {
 		super(testName);
+		
 	}
 
 	/**
@@ -25,10 +34,33 @@ public class AppTest extends TestCase {
 		return new TestSuite(AppTest.class);
 	}
 
-	/**
-	 * Rigourous Test :-)
-	 */
+	
 	public void testApp() {
 		assertTrue(true);
 	}
+	
+	@BeforeClass  
+    public static void setUpBeforeClass() throws Exception {  
+        System.out.println("before class");  
+    }  
+    @Before  
+    public void setUp() throws Exception {  
+        System.out.println("before");  
+    }
+    
+    
+    @org.junit.Test
+    public void testLowestCostIteminEachCategory(){  
+        System.out.println("test case lowestCostIteminEachCategory");
+    }
+    
+    @After  
+    public void tearDown() throws Exception {  
+        System.out.println("after");  
+    }  
+  
+    @AfterClass  
+    public static void tearDownAfterClass() throws Exception {  
+        System.out.println("after class");  
+    }  
 }
